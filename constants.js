1e6e38c66bf7b8aa021d74b16bdc0d36e1804f1b
P Paul Jonathan <ppauljonathan@gmail.com>
2025-03-31 18:45:49 +0530

    add email format validation

--- constants.js
+++ constants.js
@@ -8,3 +8,4 @@ module.exports.BCRYPT_SALT_ROUNDS = parseInt(process.env.BCRYPT_SALT_ROUNDS, 10)
 module.exports.JWT_SECRET = process.env.JWT_SECRET;
 module.exports.RESET_TOKEN_LENGTH = process.env.RESET_TOKEN_LENGTH || 16;
 module.exports.RESET_TOKEN_EXPIRY_IN_HOURS = 8;
+module.exports.EMAIL_REGEX =  /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
--- validators/user.js
+++ validators/user.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const { validatePresence, validateUniquness } = require('./commonValidators');
+const { validatePresence, validateUniquness, validateLength } = require('./commonValidators');
+const { EMAIL_REGEX } = require('../constants');
 
 module.exports.userCreationValidator = async (userData) => {
   const errors = [];
@@ -10,7 +11,20 @@ module.exports.userCreationValidator = async (userData) => {
   validatePresence('last_name', userData.last_name, errors);
 
   validatePresence('email', userData.email, errors);
+  validateEmailFormat(userData.email, errors);
   await validateUniquness('email', userData.email, 'users', errors);
 
+  validateLength('password', userData.password, { min: 8 }, errors);
+  validateLength('password_confirmation', userData.password, { min: 8 }, errors);
   return errors;
 };
+
+function validateEmailFormat(email, errors) {
+  if(email.match(EMAIL_REGEX)) { return; }
+
+  errors.push({
+    code: 422,
+    message: 'email is not in valid format. example: abc@def.com',
+    location: 'email'
+  });
+}
