f8d0e02009bce7581c6fea59af6d05191ff22040
P Paul Jonathan <ppauljonathan@gmail.com>
2025-07-10 20:56:57 +0530

    change some tabs to spaces

--- graphql/course/inputTypes.js
+++ graphql/course/inputTypes.js
@@ -1,26 +1,25 @@
 'use strict';
 
 const courseCreateInputType = `
-	input CourseCreateInput {
-		name: String!,
-		description: String!,
+  input CourseCreateInput {
+    name: String!,
+    description: String!,
     live: Boolean!
 	}
 `;
 
 const courseUpdateInputType = `
-	input CourseUpdateInput {
-		id: ID!,
-		name: String!,
-		description: String!,
+  input CourseUpdateInput {
+    id: ID!,
+    name: String!,
+    description: String!,
     live: Boolean,
     chapter_order: [Int]
-	}
+  }
 `;
 
 const inputTypes =
-	courseCreateInputType +
-	courseUpdateInputType
-;
+  courseCreateInputType +
+  courseUpdateInputType;
 
 module.exports = inputTypes;
